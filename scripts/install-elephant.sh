77849288ac1dde020b0f48a8d567f4293e960bb0
Allu <aleksi.ojansivu@gmail.com>
2026-05-09 12:31:38 +0300

    added elephant providers

--- scripts/install-elephant.sh
+++ scripts/install-elephant.sh
@@ -5,3 +5,19 @@ yay -S --noconfirm --needed elephant
 yay -S --noconfirm --needed elephant-desktopapplications
 
 yay -S --noconfirm --needed elephant-providerlist
+
+yay -S --noconfirm --needed elephant-calc
+
+yay -S --noconfirm --needed elephant-clipboard
+
+yay -S --noconfirm --needed elephant-files
+
+yay -S --noconfirm --needed elephant-menus
+
+yay -S --noconfirm --needed elephant-providerlist
+
+yay -S --noconfirm --needed elephant-runner
+
+yay -S --noconfirm --needed elephant-symbols
+
+yay -S --noconfirm --needed elephant-websearch
